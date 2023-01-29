43e5ee9f04b288f70236da5bd2c926c81e7c0901
chenxingzhi <chen.xingzhi@beicaizs.com>
2023-01-29 15:46:51 +0800

    feat: 项目初始化

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'standard-with-typescript',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+}
